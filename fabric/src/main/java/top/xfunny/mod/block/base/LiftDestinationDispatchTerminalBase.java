79b44849102230d41575e2706ea275ce7c796c7c
X-Funny <lts15087710799@outlook.com>
2025-07-13 16:37:32 +0800

    Update LiftDestinationDispatchTerminalBase.java

--- fabric/src/main/java/top/xfunny/mod/block/base/LiftDestinationDispatchTerminalBase.java
+++ fabric/src/main/java/top/xfunny/mod/block/base/LiftDestinationDispatchTerminalBase.java
@@ -349,11 +349,8 @@ public abstract class LiftDestinationDispatchTerminalBase extends BlockExtension
             //step5:呼叫电梯
             if (confirmTrackPosition[0] != null) {
                 hasButtonsClient(confirmTrackPosition[0], (floor, lift) -> {
-                    final Vector position = lift.getPosition((floorPosition1, floorPosition2) -> ItemLiftRefresher.findPath(new World(world.data), floorPosition1, floorPosition2));
-                    Position liftPos = new Position((long) position.x, (long) position.y, (long) position.z);
-
                     //避免重复开门
-                    if (lift.getDoorValue() != 0 && lift.getFloorIndex(liftPos) != lift.getFloorIndex(Init.blockPosToPosition(confirmTrackPosition[0]))) {
+                    if (lift.getDoorValue() == 0) {
                         final PressLift pressLift = new PressLift();
                         pressLift.add(Init.blockPosToPosition(confirmTrackPosition[0]), data.liftDirection);
                         InitClient.REGISTRY_CLIENT.sendPacketToServer(new PacketPressLiftButton(pressLift));
